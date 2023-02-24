cf3d750da93f610153a6ed094c44060a54b6e1d3
Timothee <timothee.viard@infotel.com>
2023-02-24 14:50:41 +0100

    completion & work

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const hello2 = "hello";
 console.log("Hello world");
- 
+
+function greetName(name = "anonymous") {
+	if (name) {
+		console.log(name);
+	}
+	console.log("hi, " + name.toUpperCase());
+}
 
 export const hello = "world";
 console.log("ok");
+greetName("ok");
